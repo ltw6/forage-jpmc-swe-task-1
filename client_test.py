68f1cc1945cf28cfc05d060a3910773902b2e1c7
ltw6 <115619436+ltw6@users.noreply.github.com>
2023-09-12 18:32:05 +0100

    Update client_test.py

--- client_test.py
+++ client_test.py
@@ -8,6 +8,17 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    price = []
+    stock = [quote['stock'] for quote in quotes]
+    top_asks = [quote['top_ask'] for quote in quotes]
+    ask_price = ([top_ask['price'] for top_ask in top_asks])
+    top_bids = [quote['top_bid'] for quote in quotes]
+    bid_price = [top_bid['price'] for top_bid in top_bids]
+    for i in range(0, len(bid_price)):
+      tempPrice = (bid_price[i]+ask_price[i]) / 2
+      price.append(tempPrice)
+    return stock, price
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,7 +26,18 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    price = []
+    stock = [quote['stock'] for quote in quotes]
+    top_asks = [quote['top_ask'] for quote in quotes]
+    ask_price = ([top_ask['price'] for top_ask in top_asks])
+    top_bids = [quote['top_bid'] for quote in quotes]
+    bid_price = [top_bid['price'] for top_bid in top_bids]
+    for i in range(0, len(bid_price)):
+      tempPrice = (bid_price[i]+ask_price[i]) / 2
+      price.append(tempPrice)
+    if price[1] == 0:
+      return
+    return price[0] / price[1]
 
   """ ------------ Add more unit tests ------------ """
 
